4f0939f44dd0f17fa83e64b1af3803da5093cce4
ssssshel <angelarteaga989@gmail.com>
2023-02-14 00:18:45 -0500

    0.1.8 dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,41 @@
 import { BaseErrorInput, BaseInput, BaseSuccessfullInput, GenericClientErrorResponse, GenericInformativeResponse, GenericRedirectionResponse, GenericServerErrorResponse, GenericSuccessfullResponse } from "./src/interfaces/bases";
 import { StatusCode1xx, StatusCode2xx, StatusCode3xx, StatusCode4xx, StatusCode5xx } from "./utils/state_codes";
 import { Response2xxOpt, Response3xxOpt, Response4xxOpt } from "./src/methods/options_pattern";
+/**
+ *
+ * @param statusCode HTTP Status Code
+ * @param input
+ *
+ */
 export declare function Response1xxInformative(statusCode: StatusCode1xx, input: BaseInput): GenericInformativeResponse;
+/**
+ *
+ * @param statusCode HTTP Status Code
+ * @param input
+ * @param statusOptions
+ * @returns
+ */
 export declare function Response2xzSuccessfull(statusCode: StatusCode2xx, input?: BaseSuccessfullInput, statusOptions?: Response2xxOpt): GenericSuccessfullResponse;
+/**
+ *
+ * @param statusCode HTTP Status Code
+ * @param input
+ * @param statusOptions
+ * @returns
+ */
 export declare function Response3xxRedirection(statusCode: StatusCode3xx, input?: BaseInput, statusOptions?: Response3xxOpt): GenericRedirectionResponse;
+/**
+ *
+ * @param statusCode HTTP Status Code
+ * @param input
+ * @param statusOptions
+ * @returns
+ */
 export declare function Response4xxClientError(statusCode: StatusCode4xx, input?: BaseErrorInput, statusOptions?: Response4xxOpt): GenericClientErrorResponse;
+/**
+ *
+ * @param statusCode HTTP Status Code
+ * @param input
+ * @returns
+ */
 export declare function Response5xxServerError(statusCode: StatusCode5xx, input?: BaseErrorInput): GenericServerErrorResponse;
--- dist/src/interfaces/bases.d.ts
+++ dist/src/interfaces/bases.d.ts
@@ -1,6 +1,12 @@
 import { StatusCode1xx, StatusCode2xx, StatusCode3xx, StatusCode4xx, StatusCode5xx } from "../../utils/state_codes";
 interface SuccessErrorProps {
+    /**
+     * Indicates if the request was successful
+     */
     success: boolean;
+    /**
+     * Indicates if the request had an error
+     */
     error: boolean;
 }
 export interface BaseInput {
@@ -77,47 +83,169 @@ export interface IBasicState207 {
     detail?: string;
 }
 export interface GenericRedirectionResponse extends BaseInput {
+    /**
+     * HTTP Status Code
+     */
     httpStatus: StatusCode3xx;
+    /**
+     * 300 ONLY | Array of options
+     * @example options: [{ name: "Option 1", description: "Description of option 1" }, { name: "Option 2", description: "Description of option 2" }]
+     */
     options?: any[];
-    sources?: ISources301[];
+    /**
+     * 301 ONLY | Object with the old and new source
+     */
+    sources?: ISources301;
+    /**
+     * Refirect for multiple states:
+     * - 302 => Temporary redirect URL
+     * - 303 => URL to which the GET request should be made
+     * - 307 => Temporary redirect URL that should be queried with the same HTTP method
+     * - 308 => Permanent redirect URL that should be queried with tha same HTTP method
+     */
     redirectUrl?: string;
+    /**
+     * 305 ONLY | URL of the proxy
+     */
     proxyUrl?: string;
 }
 export interface ISources301 {
+    /**
+     * Old source
+     */
     oldSource: string;
+    /**
+     * New source
+     */
     newSource: string;
 }
 export interface BaseErrorInput extends BaseInput {
+    /**
+     * Error/s that the server will send to the client
+     */
     errors?: any;
+    /**
+     * Error name
+     * @example errorName: "InvalidParameterError"
+     */
     errorName?: string;
+    /**
+     *  Error code
+     * @example errorCode: "INVALID_PARAMETER"
+     */
     errorCode?: string;
+    /**
+     * Error description
+     * @example errorDescription: "The parameter 'potato' is invalid"
+     */
     errorDescription?: string;
 }
 export interface GenericClientErrorResponse extends BaseErrorInput, SuccessErrorProps {
+    /**
+     * HTTP Status Code
+     */
     httpStatus: StatusCode4xx;
+    /**
+     *  Client error details
+     */
     errorDetails?: ClientErrorDetails;
 }
 export interface ClientErrorDetails {
+    /**
+     * 404 ONLY | Resource that was not found
+     * @example notFoundResource: "api/potato"
+     */
     notFoundResource?: string;
+    /**
+     * 405 ONLY | Allowed methods
+     * @example allowedMethods: ["GET", "POST"]
+     */
     allowedMethods?: string[];
+    /**
+     * 406 ONLY | Allowed representations
+     * @example allowedRepresentations: ["application/json", "application/xml"]
+     */
     allowedRepresentations?: string[];
+    /**
+     * 407 ONLY | Proxy authentication type
+     * @example authenticationType: "Basic"
+     */
     authenticationType?: string;
+    /**
+     * 407 ONLY | Domain to which auth was requested
+     * @example realm: "proxy.potato.com"
+     */
     realm?: string;
-    timeWaiting?: number;
+    /**
+     * 408 ONLY | Time waiting
+     * @example timeWaiting: "10s"
+     */
+    timeWaiting?: string;
+    /**
+     * 409 ONLY | Resource that caused the conflict
+     * @example conflictResource: "api/potato"
+     */
     conflictResource?: string;
+    /**
+     * 409 ONLY | ID of the conflict
+     * @example conflictId: "123456789"
+     */
     conflictId?: string;
+    /**
+     * 410 ONLY | Resource that is gone
+     * @example goneResource: "api/potato"
+     */
     goneResource?: string;
+    /**
+     * 410 ONLY | Reason for the resource being gone
+     * @example reason: "The resource was deleted"
+     */
     reason?: string;
+    /**
+     * 411 ONLY | Content-Length header
+     * @example requiredHeader: "Content-Length"
+     */
     requiredHeader?: string;
+    /**
+     * 413 ONLY | Max allowed payload size
+     * @example maxAllowedSize: "10MB"
+     */
     maxAllowedSize?: string;
+    /**
+     * 414 ONLY | Max allowed URI length
+     * @example maxAllowedLength: "3500"
+     */
     maxAllowedLength?: string;
+    /**
+     * 415 ONLY | Supported media types
+     * @example supportedMediaTypes: ["image/jpg", "audio/*"]
+     */
     supportedMediaTypes?: string[];
+    /**
+     * 416 ONLY | Requested content range
+     * @example requestedContentRange: "bytes=0-100"
+     */
     requestedContentRange?: string;
+    /**
+     * 416 ONLY | Supported content range
+     * @example supportedContentRange: "bytes=0-1000"
+     */
     supportedContentRange?: string;
+    /**
+     * 423 ONLY | Locked resource
+     * @example lockedResource: "api/potato"
+     */
     lockedResource?: string;
+    /**
+     * 424 ONLY | Failed dependency
+     * @example failedDependency: "api/potato"
+     */
     failedDependency?: string;
 }
 export interface GenericServerErrorResponse extends BaseErrorInput, SuccessErrorProps {
+    /**
+     * HTTP Status Code
+     */
     httpStatus: StatusCode5xx;
 }
 export {};
